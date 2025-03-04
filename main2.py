4ff45a7220e5d2da6b15fade24ea76ca94497c10
Jay <jp6631@srmist.edu.in>
2025-03-04 15:48:21 +0530

    Automating sign in on LinkedIn app

--- main2.py
+++ main2.py
@@ -1,6 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import time
+
+
+ACCOUNT_EMAIL="shivankarjay@gmail.com"
+ACCOUNT_PASSWORD="jay@135790"
 
 
 chrome_options = webdriver.ChromeOptions()
@@ -9,10 +14,18 @@ chrome_options.add_experimental_option("detach", True)
 # Create and configure the Chrome webdriver
 driver = webdriver.Chrome(options=chrome_options)
 
-# Navigate to Wikipedia
-driver.get("https://orteil.dashnet.org/experiments/cookie/")
-cookie = driver.find_element(By.ID, value="#cookie")
-cookie.click()
+# Navigate to linkedin
+driver.get("https://www.linkedin.com/jobs/search/?currentJobId=4156901053&geoId=114806696&keywords=android%20developer%20jobs&origin=JOB_SEARCH_PAGE_LOCATION_AUTOCOMPLETE&refresh=true")
+time.sleep(2)
+sign_in_button  = driver.find_element(by=By.XPATH, value='//*[@id="base-contextual-sign-in-modal"]/div/section/div/div/div/div[2]')
+sign_in_button.click()
 
-# Sending keyboard input to Selenium
+time.sleep(2)
+email=driver.find_element(by=By.XPATH,value='//*[@id="base-sign-in-modal_session_key"]')
+email.send_keys("shivankarjay@gmail.com")
+password=driver.find_element(By.XPATH,value='//*[@id="base-sign-in-modal_session_password"]')
+password.send_keys("jay@135790")
 
+time.sleep(2)
+sign_in_2 = driver.find_element(By.XPATH,value='//*[@id="base-sign-in-modal"]/div/section/div/div/form/div[2]/button')
+sign_in_2.click()
